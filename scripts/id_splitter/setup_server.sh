b395939fbf9c380d95f7787c78b1b4812cf96f4b
kshitijavis <kshitij.avi.s@gmail.com>
2022-04-26 10:36:44 -0500

    Add CUDA requirements and fix typo

--- scripts/id_splitter/setup_server.sh
+++ scripts/id_splitter/setup_server.sh
@@ -1,6 +1,9 @@
 git clone https://github.com/Forward-UIUC-2022S/kshitij-sinha-find-papers-by-keyword.git find_papers;
 cd find_papers;
+git checkout id-servers;
 python3 -m venv .venv;
 source .venv/bin/activate;
 pip3 install -r requirements.txt;
-somehow get all required data (DigiCertGlobal..., PaperIds.pickle, .env with passwords to database);
+
+# Maybe scp files from local to remove server
+scp {DigiCertGlobalRootCA.crt.pem,.env,small_data/PaperIds.pickle} ksinha7@Osprey2.csl.illinois.edu:~/find_papers/
