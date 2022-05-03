2b7391954e88a2536f97561f360b4f2be7cc2bd3
kshitijavis <kshitij.avi.s@gmail.com>
2022-05-03 01:27:30 -0500

    Update move_files to go through list of servers

--- scripts/id_splitter/move_files.sh
+++ scripts/id_splitter/move_files.sh
@@ -1,1 +1,8 @@
-scp {DigiCertGlobalRootCA.crt.pem,.env,data/PaperIds.pickle,scripts/id_splitter/gen_embs_server.sh,scripts/id_splitter/setup_server.sh,data/golden_words.csv,data/keyword_embs.pickle,data/other_freqs.pickle} $1:~
+FILENAME="data/servers.txt"
+
+LINES=$(cat $FILENAME)
+
+for LINE in $LINES
+do
+    scp {DigiCertGlobalRootCA.crt.pem,.env,data/PaperIds.pickle,scripts/id_splitter/gen_embs_server.sh,scripts/id_splitter/setup_server.sh,data/golden_words.csv,data/keyword_embs.pickle,data/other_freqs.pickle} fdlazure@$LINE:~
+done
