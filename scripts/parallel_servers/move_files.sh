72abcbd60d0471f26a4e370fc53a9c0c7902d5f0
Kshitij Sinha <kshitij.avi.s@gmail.com>
2022-05-10 16:12:11 -0500

    Remove datafile from move script

--- scripts/parallel_servers/move_files.sh
+++ scripts/parallel_servers/move_files.sh
@@ -7,7 +7,7 @@ LINES=$(cat $FILENAME)
 for LINE in $LINES
 do
     echo "Moving files to server " $LINE
-    sshpass -p $1 scp {DigiCertGlobalRootCA.crt.pem,.env,data/PaperIds.pickle,scripts/id_splitter/gen_embs_server.sh,data/golden_words.csv,data/keyword_embs.pickle,data/other_freqs.pickle,data/db_keywords.json} fdlazure@$LINE:~ &
+    sshpass -p $1 scp {DigiCertGlobalRootCA.crt.pem,.env,data/PaperIds.pickle,data/golden_words.csv,data/keyword_embs.pickle,data/other_freqs.pickle,data/db_keywords.json} fdlazure@$LINE:~ &
 done
 wait
 echo "Finished moving all files"
