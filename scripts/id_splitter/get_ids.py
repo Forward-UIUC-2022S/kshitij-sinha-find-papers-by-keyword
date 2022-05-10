4e94917e468a2a678ee6d2e6642e6ff095e81616
kshitijavis <kshitij.avi.s@gmail.com>
2022-05-10 04:24:57 -0500

    Update script that moves papers and get ids

--- scripts/id_splitter/get_ids.py
+++ scripts/id_splitter/get_ids.py
@@ -1,27 +1,18 @@
 import pickle
 import mysql.connector
-import os
-import dotenv
+import src.database.db_conn_factory as db_factory
 
 def main():
-    dotenv.load_dotenv()
-    mag_db = mysql.connector.connect(
-        user=os.getenv('MYSQL_USER'), 
-        password=os.getenv('MYSQL_PASS'), 
-        host="mag-2020-09-14.mysql.database.azure.com", 
-        port=3306, 
-        database=os.getenv('MYSQL_DB'), 
-        ssl_ca="DigiCertGlobalRootCA.crt.pem", 
-        ssl_disabled=False
-    )
+    mag_db = db_factory.get_forward_db()
 
-    id_file = "small_data/PaperIds.pickle"
-    sql = "SELECT PaperId FROM mag_2020_09_14.papers"
+    id_file = "data/PaperIds.pickle"
+    sql = "SELECT id FROM Publication"
 
     with mag_db.cursor() as cur, open(id_file, "wb") as out_file:
         cur.execute(sql)
         ids = cur.fetchall()
         ids = [i[0] for i in ids]
+
         pickle.dump(ids, out_file)
 
 
--- scripts/move_mag_database.py
+++ scripts/move_mag_database.py
@@ -3,7 +3,7 @@ import src.database.db_conn_factory as db_factory
 mag_conn = db_factory.get_azure_mag_db()
 forward_conn = db_factory.get_forward_db()
 
-limit = 4000
+limit = 1000000
 
 with mag_conn.cursor() as mag_cur, forward_conn.cursor() as forward_cur:
     get_papers_sql = f"""
@@ -15,11 +15,14 @@ with mag_conn.cursor() as mag_cur, forward_conn.cursor() as forward_cur:
     mag_cur.execute(get_papers_sql)
     paper_data = mag_cur.fetchall()
 
-    print(paper_data[:5])
+    print(f"Fetched {len(paper_data)} from Azure")
+
     save_papers_sql = """
         REPLACE INTO Publication (id, title, abstract, citations)
         VALUES (%s, %s, %s, %s)
     """
     forward_cur.executemany(save_papers_sql, paper_data)
     forward_conn.commit()
+
+    print(f"Done saving to Owl")
 
