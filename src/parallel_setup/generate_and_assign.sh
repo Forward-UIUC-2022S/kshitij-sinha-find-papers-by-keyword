b4068019e37a0ed3cceb207ac2de0b196d08318f
Kshitij Sinha <kshitij.avi.s@gmail.com>
2022-05-17 18:18:48 -0500

    Fix filepaths

--- src/parallel_setup/generate_and_assign.sh
+++ src/parallel_setup/generate_and_assign.sh
@@ -1,9 +1,9 @@
 # Argument 1: Location of SSH Key, Argument 2: Number of servers
 
-SSH_KEY_LOC = $1
-NUM_SERVERS = $2
+SSH_KEY_LOC=$1
+NUM_SERVERS=$2
 
-bash pipeline/move_files.sh $1
-bash pipeline/setup_servers.sh $1
-bash pipeline/gen_embs_servers.sh $1 $2
-bash pipeline/assign_embs_server.sh $1
+bash src/parallel_setup/pipeline/move_files.sh $SSH_KEY_LOC
+bash src/parallel_setup/setup_servers.sh $SSH_KEY_LOC
+bash src/parallel_setup/gen_embs_servers.sh $SSH_KEY_LOC $NUM_SERVERS
+bash src/parallel_setup/assign_embs_server.sh $SSH_KEY_LOC
